c52cefaff5677d9e4a794834d0740a15db9d3633
FranciscoForesi <francisco.foresi98@outlook.com>
2020-06-10 17:04:40 +0200

    Fixed Entity bug with persistence annotations in ricette-seguite

--- asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/model/Connessione.java
+++ asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/model/Connessione.java
@@ -1,13 +1,17 @@
 package asw.instagnam.ricetteseguite.domain.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 import lombok.*; 
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Connessione {
-
+    
+	@Id
+	@GeneratedValue
 	private Long id; 
 	private String follower; 
 	private String followed; 
--- asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/model/Ricetta.java
+++ asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/model/Ricetta.java
@@ -1,13 +1,17 @@
 package asw.instagnam.ricetteseguite.domain.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 import lombok.*;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Ricetta {
-
+	
+	@Id
+	@GeneratedValue
 	private Long id; 
 	private String autore; 
 	private String titolo; 
