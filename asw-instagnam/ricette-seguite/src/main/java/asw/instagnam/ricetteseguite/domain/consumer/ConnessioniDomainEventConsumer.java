718c8e63f6d1b3e3d2bf9bb4680c441eb61d010c
FranciscoForesi <francisco.foresi98@outlook.com>
2020-06-11 13:07:11 +0200

    Edited Connessione consumer for saving in ricetteseguiterepository

--- asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/consumer/ConnessioniDomainEventConsumer.java
+++ asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/consumer/ConnessioniDomainEventConsumer.java
@@ -1,20 +1,32 @@
 package asw.instagnam.ricetteseguite.domain.consumer;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import asw.instagnam.common.api.event.DomainEvent;
+import asw.instagnam.ricetteseguite.domain.model.Ricetta;
 import asw.instagnam.ricetteseguite.domain.model.Connessione;
+import asw.instagnam.ricetteseguite.domain.model.RicettaSeguita;
 import asw.instagnam.ricetteseguite.domain.repository.ConnessioniRepository;
+import asw.instagnam.ricetteseguite.domain.repository.RicetteRepository;
+import asw.instagnam.ricetteseguite.domain.repository.RicetteSeguiteRepository;
 import connessioni.service.api.event.ConnessioneCreatedEvent;
 
 
 @Service
 public class ConnessioniDomainEventConsumer implements DomainEventConsumer {
-	
+
 	@Autowired
 	private ConnessioniRepository connessioniRepository;
-	
+	@Autowired
+	private RicetteSeguiteRepository ricetteSeguiteRepository;
+	@Autowired
+	private RicetteRepository ricetteRepository;
+
 	@Override
 	public void onEvent(DomainEvent event) {
 		if (event instanceof ConnessioneCreatedEvent) {
@@ -23,15 +35,23 @@ public class ConnessioniDomainEventConsumer implements DomainEventConsumer {
 			System.out.println("Evento non riconosciuto\n");
 		}
 	}
-	
+
 	private void handleConnessioneCreatedEvent(DomainEvent event) {
 		ConnessioneCreatedEvent c = (ConnessioneCreatedEvent) event;
 		// Create new connection from the event
 		Connessione connessione = new Connessione(c.getId(), c.getFollower(), c.getFollowed());
-		
+		// Finding all followed user's ricette
+		String autore = connessione.getFollowed();
+		Collection<Ricetta> ricette = ricetteRepository.findAllByAutore(autore);
+		List<RicettaSeguita> ricetteSeguite = new ArrayList<>();
+		ricette.stream().forEach(ricetta -> ricetteSeguite.add(new RicettaSeguita(connessione.getFollower(),
+				ricetta.getId(), ricetta.getAutore(), ricetta.getTitolo())));
+
+        // Save new connessione in all repositories
 		connessioniRepository.save(connessione);
-		
+		ricetteSeguite.stream().forEach(ricettaSeguita -> ricetteSeguiteRepository.save(ricettaSeguita));
+
 
 	}
-	
+
 }
