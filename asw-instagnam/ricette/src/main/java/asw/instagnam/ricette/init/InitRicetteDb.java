99bc341fe6516cafd7b149b16fd0f83cd063acaa
piervy <piervy10@hotmail.it>
2020-06-11 18:40:39 +0200

    Fixed Bug on initRicetteDb (Ricette seguite non riconosceva le due ricette di cristiano)

--- asw-instagnam/ricette/src/main/java/asw/instagnam/ricette/init/InitRicetteDb.java
+++ asw-instagnam/ricette/src/main/java/asw/instagnam/ricette/init/InitRicetteDb.java
@@ -13,12 +13,15 @@ public class InitRicetteDb implements CommandLineRunner {
 	@Autowired 
 	private RicetteService ricetteService; 
 
-	public void run(String[] args) {		
+	public void run(String[] args) {	
+		ricetteService.createRicetta( "Benedetta", "Tonno e fagioli", "Unisci il tonno con i fagioli" );
 		ricetteService.createRicetta( "Cristiano", "Panino al prosciutto", "Metti il prosciutto nel panino" );	
 		ricetteService.createRicetta( "Cristiano", "Pizza e mortazza", "Metti la mortadella nella pizza" );	
+		ricetteService.createRicetta( "Cristiano", "Pizza e funghi", "Metti i funghi nella pizza" );	
+		ricetteService.createRicetta( "Cristiano", "Pasta", "Metti la pasta a bollire" );	
 		ricetteService.createRicetta( "Gennaro", "Tonno e fagioli", "Unisci i fagioli con il tonno" );	
 		ricetteService.createRicetta( "Antonino", "Pizza margherita", "... e alla fine inforna" );	
-		ricetteService.createRicetta( "Benedetta", "Tonno e fagioli", "Unisci il tonno con i fagioli" );	
+			
 	}
 	
 }
