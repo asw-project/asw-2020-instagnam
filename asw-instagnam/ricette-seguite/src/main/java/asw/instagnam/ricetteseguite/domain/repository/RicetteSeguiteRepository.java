38bc7b38db975a2031bcef89777b0d381a7ed295
FranciscoForesi <francisco.foresi98@outlook.com>
2020-06-11 12:56:22 +0200

    Initial commit for complex variant

---
+++ asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/repository/RicetteSeguiteRepository.java
@@ -0,0 +1,13 @@
+package asw.instagnam.ricetteseguite.domain.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.repository.CrudRepository;
+
+import asw.instagnam.ricetteseguite.domain.model.RicettaSeguita;
+
+public interface RicetteSeguiteRepository extends CrudRepository<RicettaSeguita,Long> {
+	
+	public Collection<RicettaSeguita> findAllByFollower(String follower);
+
+}
